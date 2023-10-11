e7c05c25328c62dcc79105423cde7e0f025317d9
Zafer Balkan <zafer@zaferbalkan.com>
2023-10-11 11:57:26 +0300

    Updated publish script

--- publish.ps1
+++ publish.ps1
@@ -1,9 +1,8 @@
-#Requires -RunAsAdministrator
 #Requires -Version 5
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-$apiKey = Get-Content -Path.\.apikey -Raw
+$apiKey = (Get-Content -Path.\.apikey -Raw).replace("`n", ', ').replace("`r", ', ')
 $module = '.\slmgr-ps.psd1'
 
 Publish-Module -Name $module -NuGetApiKey $apiKey
