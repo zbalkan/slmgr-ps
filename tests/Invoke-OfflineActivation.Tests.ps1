689675bc8bde77f31b6a20758efd04ef9c1929e6
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-11 16:18:28 +0300

    Verify offline activation outcome

---
+++ tests/Invoke-OfflineActivation.Tests.ps1
@@ -0,0 +1,86 @@
+BeforeAll {
+    . $PSScriptRoot/../src/Private/LicenseStatusCode.ps1
+    . $PSScriptRoot/../src/Private/Invoke-OfflineActivation.ps1
+
+    function Get-LicenseStatus {}
+    function Get-WindowsLicensingProduct {}
+    function Get-OfflineInstallationId {}
+    function Invoke-SppCimMethod {
+        [CmdletBinding()]
+        param(
+            [Parameter(Mandatory, ValueFromPipeline)]$InputObject,
+            [Parameter(Mandatory)][string]$MethodName,
+            [hashtable]$Arguments
+        )
+    }
+
+    $script:MockCimSession = New-MockObject -Type 'Microsoft.Management.Infrastructure.CimSession'
+    $script:Service = New-CimInstance -ClassName SoftwareLicensingService -ClientOnly
+    $script:Product = New-CimInstance -ClassName SoftwareLicensingProduct -ClientOnly
+}
+
+Describe 'Invoke-OfflineActivation' {
+    BeforeEach {
+        $script:StatusCall = 0
+        Mock Get-LicenseStatus {
+            $script:StatusCall++
+            if ($script:StatusCall -eq 1)
+            {
+                [PSCustomObject]@{ LicenseStatus = [LicenseStatusCode]::Unlicensed; Activated = $false }
+            }
+            else
+            {
+                [PSCustomObject]@{ LicenseStatus = [LicenseStatusCode]::Licensed; Activated = $true }
+            }
+        }
+        Mock Get-WindowsLicensingProduct { $script:Product }
+        Mock Get-OfflineInstallationId { [PSCustomObject]@{ OfflineInstallationId = '123456789' } }
+        Mock Invoke-SppCimMethod {}
+    }
+
+    It 'normalizes and deposits the confirmation ID' {
+        Invoke-OfflineActivation -CimSession $script:MockCimSession -Service $script:Service `
+            -ConfirmationId '123456-123456-123456-123456-123456-123456-123456-123456-123456'
+
+        Should -Invoke Invoke-SppCimMethod -ParameterFilter {
+            $MethodName -eq 'DepositOfflineConfirmationId' -and
+            $Arguments.ConfirmationId -eq '123456123456123456123456123456123456123456123456123456'
+        } -Times 1
+    }
+
+    It 'refreshes and verifies the final license status' {
+        Invoke-OfflineActivation -CimSession $script:MockCimSession -Service $script:Service `
+            -ConfirmationId ('1' * 54)
+
+        Should -Invoke Invoke-SppCimMethod -ParameterFilter { $MethodName -eq 'RefreshLicenseStatus' } -Times 1
+        Should -Invoke Get-LicenseStatus -Times 2
+    }
+
+    It 'throws when the final state is not activated' {
+        Mock Get-LicenseStatus {
+            [PSCustomObject]@{ LicenseStatus = [LicenseStatusCode]::Notification; Activated = $false }
+        }
+
+        { Invoke-OfflineActivation -CimSession $script:MockCimSession -Service $script:Service `
+                -ConfirmationId ('1' * 54) } | Should -Throw -ExpectedMessage '*Offline activation failed*'
+    }
+
+    It 'accepts extended grace with a warning' {
+        $script:StatusCall = 0
+        Mock Get-LicenseStatus {
+            $script:StatusCall++
+            if ($script:StatusCall -eq 1)
+            {
+                [PSCustomObject]@{ LicenseStatus = [LicenseStatusCode]::Unlicensed; Activated = $false }
+            }
+            else
+            {
+                [PSCustomObject]@{ LicenseStatus = [LicenseStatusCode]::ExtendedGrace; Activated = $false }
+            }
+        }
+
+        Invoke-OfflineActivation -CimSession $script:MockCimSession -Service $script:Service `
+            -ConfirmationId ('1' * 54) -WarningVariable warning
+        $warning | Should -Match 'extended grace'
+    }
+}
