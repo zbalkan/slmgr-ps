480fd536470be4ea9c0bf434a7d3b84403c3c6aa
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-11 16:18:21 +0300

    Validate SPP method class ownership
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- src/Private/Invoke-SppCimMethod.ps1
+++ src/Private/Invoke-SppCimMethod.ps1
@@ -10,6 +10,38 @@ function Invoke-SppCimMethod
     )
     Process
     {
+        $serviceMethods = @(
+            'ClearKeyManagementServiceMachine',
+            'ClearKeyManagementServicePort',
+            'ClearProductKeyFromRegistry',
+            'DisableKeyManagementServiceHostCaching',
+            'InstallProductKey',
+            'ReArmWindows',
+            'RefreshLicenseStatus',
+            'SetKeyManagementServiceMachine',
+            'SetKeyManagementServicePort'
+        )
+        $productMethods = @(
+            'Activate',
+            'DepositOfflineConfirmationId',
+            'UninstallProductKey'
+        )
+
+        $className = $InputObject.CimClass.CimClassName
+        $expectedClass = if ($MethodName -in $serviceMethods)
+        {
+            'SoftwareLicensingService'
+        }
+        elseif ($MethodName -in $productMethods)
+        {
+            'SoftwareLicensingProduct'
+        }
+
+        if ($null -ne $expectedClass -and $className -ne $expectedClass)
+        {
+            throw "$MethodName requires $expectedClass, but received $className"
+        }
+
         $invokeParams = @{ MethodName = $MethodName }
         if ($PSBoundParameters.ContainsKey('Arguments')) { $invokeParams['Arguments'] = $Arguments }
 
