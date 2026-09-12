68f5c532ff898d4e721156dacedefafeb74bc5c9
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-12 20:16:20 +0300

    Define SPP product targeting contract
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

---
+++ src/Private/Get-SppContract.ps1
@@ -0,0 +1,66 @@
+function Get-SppContract
+{
+    [CmdletBinding()]
+    param()
+
+    return @{
+        ProductProperties = @(
+            'ApplicationID',
+            'ID',
+            'LicenseIsAddon',
+            'LicenseStatus',
+            'OfflineInstallationId',
+            'PartialProductKey'
+        )
+        Methods = @{
+            Activate = @{
+                Classes   = @('SoftwareLicensingProduct')
+                Arguments = @()
+            }
+            ClearKeyManagementServiceMachine = @{
+                Classes   = @('SoftwareLicensingService', 'SoftwareLicensingProduct')
+                Arguments = @()
+            }
+            ClearKeyManagementServicePort = @{
+                Classes   = @('SoftwareLicensingService', 'SoftwareLicensingProduct')
+                Arguments = @()
+            }
+            ClearProductKeyFromRegistry = @{
+                Classes   = @('SoftwareLicensingService')
+                Arguments = @()
+            }
+            DepositOfflineConfirmationId = @{
+                Classes   = @('SoftwareLicensingProduct')
+                Arguments = @('InstallationId', 'ConfirmationId')
+            }
+            DisableKeyManagementServiceHostCaching = @{
+                Classes   = @('SoftwareLicensingService')
+                Arguments = @()
+            }
+            InstallProductKey = @{
+                Classes   = @('SoftwareLicensingService')
+                Arguments = @('ProductKey')
+            }
+            ReArmWindows = @{
+                Classes   = @('SoftwareLicensingService')
+                Arguments = @()
+            }
+            RefreshLicenseStatus = @{
+                Classes   = @('SoftwareLicensingService')
+                Arguments = @()
+            }
+            SetKeyManagementServiceMachine = @{
+                Classes   = @('SoftwareLicensingService', 'SoftwareLicensingProduct')
+                Arguments = @('MachineName')
+            }
+            SetKeyManagementServicePort = @{
+                Classes   = @('SoftwareLicensingService', 'SoftwareLicensingProduct')
+                Arguments = @('PortNumber')
+            }
+            UninstallProductKey = @{
+                Classes   = @('SoftwareLicensingProduct')
+                Arguments = @()
+            }
+        }
+    }
+}
