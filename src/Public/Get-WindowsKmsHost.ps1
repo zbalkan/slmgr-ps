3e6dbcd355b049ee3bb16ae7cce086bea522f06a
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-13 22:57:17 +0300

    Handle optional KMS activation-disabled state
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- src/Public/Get-WindowsKmsHost.ps1
+++ src/Public/Get-WindowsKmsHost.ps1
@@ -27,31 +27,37 @@ function Get-WindowsKmsHost
                 $service = Get-KmsHostService -CimSession $session -ErrorAction Stop
 
                 $configuredPort = [uint32]$service.KeyManagementServiceListeningPort
+                $activationDisabled = $null
+                if ($null -ne $service.PSObject.Properties['KeyManagementServiceActivationDisabled'])
+                {
+                    $activationDisabled = [bool]$service.KeyManagementServiceActivationDisabled
+                }
+
                 $result = [PSCustomObject][ordered]@{
                     ComputerName                    = $c
                     IsKmsHost                       = $true
-                    ListeningPortConfigured          = ($configuredPort -ne 0)
-                    ConfiguredListeningPort          = if ($configuredPort -eq 0) { $null } else { $configuredPort }
-                    EffectiveListeningPort           = if ($configuredPort -eq 0) { [uint32]1688 } else { $configuredPort }
-                    ActivationInterval               = [uint32]$service.VLActivationInterval
-                    RenewalInterval                  = [uint32]$service.VLRenewalInterval
-                    DnsPublishing                    = if ($service.KeyManagementServiceDnsPublishing) { 'Enabled' } else { 'Disabled' }
-                    Priority                         = if ($service.KeyManagementServiceLowPriority) { 'Low' } else { 'Normal' }
-                    CurrentClientCount               = [uint32]$service.KeyManagementServiceCurrentCount
-                    RequiredClientCount              = [uint32]$service.RequiredClientCount
-                    ProductKeyId                     = $service.KeyManagementServiceProductKeyID
-                    ActivationDisabled               = [bool]$service.KeyManagementServiceActivationDisabled
-                    UnlicensedRequests               = [uint32]$service.KeyManagementServiceUnlicensedRequests
-                    LicensedRequests                 = [uint32]$service.KeyManagementServiceLicensedRequests
-                    OobGraceRequests                 = [uint32]$service.KeyManagementServiceOOBGraceRequests
-                    OotGraceRequests                 = [uint32]$service.KeyManagementServiceOOTGraceRequests
-                    NonGenuineGraceRequests          = [uint32]$service.KeyManagementServiceNonGenuineGraceRequests
-                    NotificationRequests             = [uint32]$service.KeyManagementServiceNotificationRequests
-                    TotalRequests                    = [uint32]$service.KeyManagementServiceTotalRequests
-                    FailedRequests                   = [uint32]$service.KeyManagementServiceFailedRequests
-                    DefaultListeningPort              = [uint32]1688
-                    DefaultActivationInterval         = [uint32]120
-                    DefaultRenewalInterval            = [uint32]10080
+                    ListeningPortConfigured         = ($configuredPort -ne 0)
+                    ConfiguredListeningPort         = if ($configuredPort -eq 0) { $null } else { $configuredPort }
+                    EffectiveListeningPort          = if ($configuredPort -eq 0) { [uint32]1688 } else { $configuredPort }
+                    ActivationInterval              = [uint32]$service.VLActivationInterval
+                    RenewalInterval                 = [uint32]$service.VLRenewalInterval
+                    DnsPublishing                   = if ($service.KeyManagementServiceDnsPublishing) { 'Enabled' } else { 'Disabled' }
+                    Priority                        = if ($service.KeyManagementServiceLowPriority) { 'Low' } else { 'Normal' }
+                    CurrentClientCount              = [uint32]$service.KeyManagementServiceCurrentCount
+                    RequiredClientCount             = [uint32]$service.RequiredClientCount
+                    ProductKeyId                    = $service.KeyManagementServiceProductKeyID
+                    ActivationDisabled              = $activationDisabled
+                    UnlicensedRequests              = [uint32]$service.KeyManagementServiceUnlicensedRequests
+                    LicensedRequests                = [uint32]$service.KeyManagementServiceLicensedRequests
+                    OobGraceRequests                = [uint32]$service.KeyManagementServiceOOBGraceRequests
+                    OotGraceRequests                = [uint32]$service.KeyManagementServiceOOTGraceRequests
+                    NonGenuineGraceRequests         = [uint32]$service.KeyManagementServiceNonGenuineGraceRequests
+                    NotificationRequests            = [uint32]$service.KeyManagementServiceNotificationRequests
+                    TotalRequests                   = [uint32]$service.KeyManagementServiceTotalRequests
+                    FailedRequests                  = [uint32]$service.KeyManagementServiceFailedRequests
+                    DefaultListeningPort            = [uint32]1688
+                    DefaultActivationInterval       = [uint32]120
+                    DefaultRenewalInterval          = [uint32]10080
                 }
                 $result.PSObject.TypeNames.Insert(0, 'slmgr-ps.KmsHostStatus')
                 Write-Output $result
