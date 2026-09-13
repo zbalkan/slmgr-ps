b6ae3429522688f6d57a9630c98d61c3b47396fb
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-13 20:33:14 +0300

    Test structured SPP provider outcomes
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

---
+++ tests/Invoke-SppCimMethod.Result.Tests.ps1
@@ -0,0 +1,42 @@
+BeforeAll {
+    . $PSScriptRoot/../src/Private/Get-SppContract.ps1
+    . $PSScriptRoot/../src/Private/Invoke-SppCimMethod.ps1
+
+    $script:Product = New-CimInstance -ClassName SoftwareLicensingProduct -ClientOnly
+}
+
+Describe 'Invoke-SppCimMethod result contract' {
+    It 'returns the provider result only when PassThru is requested' {
+        Mock Invoke-CimMethod { [PSCustomObject]@{ ReturnValue = 0; ProviderValue = 'ok' } }
+
+        $defaultResult = @($script:Product | Invoke-SppCimMethod -MethodName Activate)
+        $passThruResult = $script:Product | Invoke-SppCimMethod -MethodName Activate -PassThru
+
+        $defaultResult.Count | Should -Be 0
+        $passThruResult.ReturnValue | Should -Be 0
+        $passThruResult.ProviderValue | Should -Be 'ok'
+    }
+
+    It 'preserves provider failure identity and hexadecimal error code' {
+        Mock Invoke-CimMethod { [PSCustomObject]@{ ReturnValue = [uint32]0xC004F050 } }
+
+        $caught = $null
+        try
+        {
+            $script:Product | Invoke-SppCimMethod -MethodName Activate
+        }
+        catch
+        {
+            $caught = $_
+        }
+
+        $caught | Should -Not -BeNullOrEmpty
+        $caught.FullyQualifiedErrorId | Should -Match '^SppProviderMethodFailed'
+        $caught.Exception.Data['ProviderReturnValue'] | Should -Be ([uint32]0xC004F050)
+        $caught.Exception.Data['ErrorCode'] | Should -Be '0xC004F050'
+        $caught.Exception.Data['MethodName'] | Should -Be 'Activate'
+        $caught.TargetObject.MethodName | Should -Be 'Activate'
+        $caught.TargetObject.ClassName | Should -Be 'SoftwareLicensingProduct'
+        $caught.TargetObject.ErrorCode | Should -Be '0xC004F050'
+    }
+}
