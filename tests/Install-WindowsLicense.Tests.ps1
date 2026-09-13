907c54cbc5307f15962afdadecd74c15f28d0f4b
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-13 20:39:38 +0300

    Load operation result helpers in license install tests
    Signed-off-by: Zafer Balkan <zafer@zaferbalkan.com>

--- tests/Install-WindowsLicense.Tests.ps1
+++ tests/Install-WindowsLicense.Tests.ps1
@@ -1,4 +1,7 @@
 BeforeAll {
+    . $PSScriptRoot/../src/Private/New-LicensingOperationResult.ps1
+    . $PSScriptRoot/../src/Private/New-LicensingOperationError.ps1
+    . $PSScriptRoot/../src/Private/Complete-LicensingOperationBatch.ps1
     . $PSScriptRoot/../src/Public/Install-WindowsLicense.ps1
 
     function Get-LicenseFileContent { param([string[]]$Path) }
